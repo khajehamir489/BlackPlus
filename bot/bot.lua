54058b0ab86081bffe926bcabf1889fd9b2cef14
khajehamir489 <khajehamir489@gmail.com>
2016-11-04 19:20:52 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,7 +263,7 @@ function create_config( )
     "Block",
     "InGroups",
     },
-    sudo_users = {56693692,},--Sudo users
+    sudo_users = {216507730,},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[
 ]],
